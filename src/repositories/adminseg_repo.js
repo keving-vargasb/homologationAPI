d907724e82134562adc45920d37ba5ac967ad152
Kevin Vargas <kevin@vantageio.com>
2021-09-14 12:37:39 -0500

    get homologation object service

--- src/repositories/adminseg_repo.js
+++ src/repositories/adminseg_repo.js
@@ -1,10 +1,33 @@
 const Adminseg = require("../util/adminseg/adminseg");
 const HttpResponse = require("../util/http_response");
+const utils = require('../util/util');
 
 const AdminsegProvider = require("../providers/adminseg_provider");
 
+const getHomologationObject = async (applicationID) => {
+  try {
+
+    const applicationDataResponse = await utils.fetchGET({
+      url: `${process.env.API_ENDPOINT}/api/application/${applicationID}`
+    });
+
+    const applicationData = applicationDataResponse.data
+
+    if(!applicationData) return new HttpResponse("application_not_found", 200, "Application not found", applicationID);
+
+    const adminseg = new Adminseg.Adminseg(applicationData);
+    const homologationObject = await adminseg.homologationObject();
+
+    return new HttpResponse("ok", 200, "ok", homologationObject);
+  } catch (error) {
+    console.log({ error });
+    return new HttpResponse("unknown_error", 200, error.message, error);
+  }
+}
+
 const createQuoteInAdminseg = async (applicationData) => {
   try {
+    if(process.env.ENVIRONMENT !== 'DEV') return new HttpResponse("service_not_available", 401, "Service not available", null);
     const adminseg = new Adminseg.Adminseg(applicationData);
     const homologationObject = await adminseg.homologationObject();
 
@@ -25,5 +48,6 @@ const createQuoteInAdminseg = async (applicationData) => {
 };
 
 module.exports = {
+  getHomologationObject,
   createQuoteInAdminseg,
 };
--- src/routes/adminseg_routes.js
+++ src/routes/adminseg_routes.js
@@ -1,9 +1,16 @@
 const AdminsegRepo = require("../repositories/adminseg_repo");
 
 module.exports = function (app) {
+
+  app.get("/adminseg/homologation/object/:applicationID", async (req, res) => {
+    const { applicationID } = req.params;
+    const result = await AdminsegRepo.getHomologationObject(applicationID);
+    res.status(result.httpCode).json(result);
+  });
+
+  //ONLY DEV
   app.post("/adminseg/homologation", async (req, res) => {
     const { applicationData } = req.body;
-
     const result = await AdminsegRepo.createQuoteInAdminseg(applicationData);
     res.status(result.httpCode).json(result);
   });
